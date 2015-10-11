2d21c47e793dbe69c224d5328e2c296e2733c492
Cezar <cezar.a.bernardi@gmail.com>
2015-10-10 22:51:41 -0300

    Username check

--- SID-T1-Client/src/sid/t1/client/Control.java
+++ SID-T1-Client/src/sid/t1/client/Control.java
@@ -8,84 +8,82 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import sid.t1.pkginterface.ClientInterface;
 import sid.t1.pkginterface.SuperPeerInterface;
 
 /*
-Definição número das portas
-3000 = Interna peers
-20xx = PeerX para clientes
-21xx = Client registry
-*/
-
+ Definição número das portas
+ 3000 = Interna peers
+ 20xx = PeerX para clientes
+ 21xx = Client registry
+ */
 public class Control extends UnicastRemoteObject implements ClientInterface {
+
     Client userInterface;
     static SuperPeerInterface superPeer;
     String endName;
     int endNumber;
     String fullName;
-    
+
     @Override
     public void deliverMessage(String client, String message, String source) throws RemoteException {
-        userInterface.println("De '" + source + "' Para '" + client + "': " + message );
+        userInterface.println("De '" + source + "' Para '" + client + "': " + message);
     }
-    
-    public Control( Client userInterface ) throws RemoteException {
+
+    public Control(Client userInterface) throws RemoteException {
         this.userInterface = userInterface;
     }
 
-    // [BASTOS] coloquei o init() aqui pra ser chamado pelo botão 'Conectar'
     public void connect() {
+        endName = userInterface.getClientID().getText();
+        fullName = "//localhost:" + String.valueOf(ClientInterface.port + endNumber) + "/" + ClientInterface.baseName + endName;
         try {
-            // Cada instância de client precisa de um nome fixo
-            // por exemplo ClientPaulo e ClientPedro
-            // pois se deixar automático vai ter um Cliente1 para cada peer
-            // pode ser escolher na hora que da play, como um login
-            // nao precisa ter como contato, só saber o nome na hora de mandar, mas é detalhe isso
-            
-            // Se registra no Registry de porta 21xx que será lida pelo PeerXX
-            endName = userInterface.getClientID().getText();
-            
-            
-            String peers[] = Naming.list("//localhost:"+SuperPeerInterface.peerPort+"/");
-            //Se mudar pra 1 funciona se tiver 2 Peer rodando
-            Random rn = new Random();
-            endNumber = rn.nextInt() % peers.length;
-            String serverFullName = "//localhost:" + String.valueOf(SuperPeerInterface.port + endNumber) + "/" + SuperPeerInterface.baseName + endNumber;
-            superPeer = (SuperPeerInterface) Naming.lookup(serverFullName);
-            
+            Naming.lookup(fullName);
+            //already exists
+            JOptionPane.showMessageDialog(userInterface, "Username Already Bound!");
+        } catch (NotBoundException ex) {
+            try {
+                fullName = "//localhost:" + String.valueOf(ClientInterface.port + endNumber) + "/" + ClientInterface.baseName + endName;
+                Naming.rebind(fullName, this);
+                userInterface.println(fullName);
+                userInterface.println(endName + " conectado.");
+                userInterface.setConnected();
+
+                String peers[] = Naming.list("//localhost:" + SuperPeerInterface.peerPort + "/");
+                Random rn = new Random();
+                endNumber = rn.nextInt() % peers.length;
+                String serverFullName = "//localhost:" + String.valueOf(SuperPeerInterface.port + endNumber) + "/" + SuperPeerInterface.baseName + endNumber;
+                superPeer = (SuperPeerInterface) Naming.lookup(serverFullName);
+                
+            } catch (RemoteException | MalformedURLException | NotBoundException ex1) {
+                Logger.getLogger(Control.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        } catch (RemoteException | MalformedURLException ex) {
+            Logger.getLogger(Control.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void disconnect() {
+        try {
             fullName = "//localhost:" + String.valueOf(ClientInterface.port + endNumber) + "/" + ClientInterface.baseName + endName;
-            Naming.rebind(fullName, this);
-            userInterface.println(fullName);
-            userInterface.println(endName + " conectado.");
-            userInterface.setConnected();
+            Naming.unbind(fullName);
+            userInterface.println(endName + " desconectado.");
+            userInterface.setDisconnected();
         } catch (NotBoundException | RemoteException | MalformedURLException ex) {
             Logger.getLogger(Control.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void disconnect() {
-        try {
-            fullName = "//localhost:" + String.valueOf(ClientInterface.port + endNumber) + "/" + ClientInterface.baseName + endName;
-            Naming.unbind( fullName );
-            userInterface.println(endName + " desconectado.");
-            userInterface.setDisconnected();
-        }
-        catch (NotBoundException | RemoteException | MalformedURLException ex) {
-            Logger.getLogger(Control.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void sendMessage(){
+
+    public void sendMessage() {
         try {
             String clientName = ClientInterface.baseName + endName;
             String message = userInterface.getMessage().getText();
             String receiver = ClientInterface.baseName + userInterface.getReceiverID().getText();
-            
+
             superPeer.sendMessageClient(receiver, message, clientName);
-            
+
             //userInterface.getMessage().setText( "" ); // LIMPA A CAIXA DA MENSAGEM DEPOIS DE ENVIAR
-            
         } catch (RemoteException ex) {
             Logger.getLogger(Control.class.getName()).log(Level.SEVERE, null, ex);
         }
